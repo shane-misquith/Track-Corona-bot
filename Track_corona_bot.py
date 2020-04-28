138871d0debd5357e4b573d9948313f43acc2237
shane-misquith <shane.misquith@gmail.com>
2020-04-28 20:08:11 +0530

    updated Track_corona_bot.py
    fixed table values.

--- Track_corona_bot.py
+++ Track_corona_bot.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-from table_data import html
+from data import html
 
 # URL = "https://www.mohfw.gov.in/"
 # response = requests.get(URL)
@@ -10,11 +10,19 @@ extract_contents = lambda row: [x.text for x in row]
 t_header = extract_contents(soup.find_all('th'))
 
 row = []
-all_rows = soup.find_all('tr')
+# all_rows = soup.find_all('tr')
 for all_rows in soup.find_all('tr'):
 	row_data = extract_contents(all_rows.find_all('td'))
-	if len(row_data) == 5 :
-		# row_data = ['', *row_data]
+	# print(row_data)
+	# if len(row):
+	if len(row_data) == 1:
+		pass
+	elif len(row_data) == 4 :
+		row_data = ['', *row_data]
+		row.append(row_data)
+	elif len(row_data) == 5:	
 		row.append(row_data)
 
-print(row)
+for i in row:
+	print(i)
+# print(row)
